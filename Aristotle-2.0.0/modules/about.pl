3bb301f2962785bd63e23d525293aa48f38fe426
Zarco <luiz.zarco@gmx.com>
2016-06-10 07:12:11 -0300

    Update - Solve Warnings

---
+++ Aristotle-2.0.0/modules/about.pl
@@ -0,0 +1,42 @@
+% Aristotle's About
+
+:- module(about,[about/0]).
+
+about :-
+   not( exists_frame ),
+   new( Frame,  frame( 'About - Aristotle' ) ),
+   send( Frame, append, new( Dialog, dialog ) ),
+   send( Dialog, append, text('Aristotle - Computational Argumentation')),
+   send( Dialog, append, new( Editor, editor( new( text_buffer ), 64,10) ) ),
+   send( Dialog, append, button( close, message( @prolog, close ) ) ),
+   send_list( Editor, append,
+   ['Aristotle - Computational Argumentation (Version 2)\n
+     Copyright (c) 2013-2016 PEREIRA, SANTOS & LIRA.\n
+     Available at http://www.ime.usp.br/~slago/aristotle.zip\n
+     This is free software. There is ABSOLUTELY NO WARRANTY.
+     Read LICENSE.txt for more information.
+       '] ),
+   var( about:frame, Frame ),
+   send( Editor, font, font( screen, normal, 12 ) ),
+   send( Editor, caret, 0),
+   send( Editor, editable, @off),
+   send( Frame,  open, point( 350, 350 ) ).
+
+exists_frame :-
+   var( about:frame, Frame ),
+   catch( send( Frame, expose ), Error, true ),
+   var( Error ).
+
+close :-
+   var( about:frame, Frame ),
+   send( Frame, destroy ).
+
+
+
+
+
+
+
+
+
+
